5a575ae44c15fcb904dc4b6c2712d4f459ebec27
Reuben Steenekamp <reuben.steenekamp@gmail.com>
2016-10-01 01:08:54 +0200

    Fixed SPI by clearing ANSELC on all SPI pins.

--- micro/spi.c
+++ micro/spi.c
@@ -1,7 +1,7 @@
 #include <spi.h>
+#include <timing.h>
 
 void init_spi() {
-
   // Put SDO on RC2
   APFCON0bits.SDOSEL = 0;
 
@@ -9,15 +9,18 @@ void init_spi() {
 
   //nCS
   TRISCbits.TRISC3 = 0;
+  ANSELCbits.ANSC3 = 0;
   PORTCbits.RC3 = 1;
   //SCK
   TRISCbits.TRISC0 = 0;
+  ANSELCbits.ANSC0 = 0;
   PORTCbits.RC0 = 0;
   //SDI
   TRISCbits.TRISC1 = 1;
-  PORTCbits.RC1 = 0;
+  ANSELCbits.ANSC1 = 0;
   //SDO
   TRISCbits.TRISC2 = 0;
+  ANSELCbits.ANSC2= 0;
   PORTCbits.RC2 = 0;
 
 
@@ -30,6 +33,10 @@ void init_spi() {
 
   // Input sampled at middle
   SSP1STATbits.SMP=0;
+
+  // Why not
+  SSP1CON1bits.WCOL=0;
+  SSP1CON1bits.SSPOV=0;
 
   // Enable
   SSP1CON1bits.SSPEN = 1;
@@ -49,7 +56,7 @@ void send_spi_raw(uint8_t *tx, uint8_t tx_len, uint8_t *rx, uint8_t rx_len) {
   uint8_t len = rx_len > tx_len ? rx_len : tx_len;
   for(uint8_t i=0; i<len; ++i) {
     SSP1BUF = i < tx_len ? tx[i] : 0;
-    while(!SSPSTATbits.BF);
+    while(!SSP1STATbits.BF);
     uint8_t tmp = SSP1BUF;
     if(i < rx_len) rx[i] = tmp;
   }
